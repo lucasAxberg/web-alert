7559cab2be6f681f06f84ac8a9b60886e21ef9e6
Lucas Axberg <lucas.axberg@gmail.com>
2025-02-05 08:26:02 +0100

    edit: close window after click for + and - keys to work

--- src/popup/script.js
+++ src/popup/script.js
@@ -21,8 +21,10 @@ function add_function() {
 	// browser.scripting.executeScript({file: "/src/main.js"})
 	browser.tabs.query({active: true, currentWindow: true})
 	.then((tabs) => {
-		browser.tabs.sendMessage(tabs[0].id, {action: "enableEventListener"})
-		// window.close()
+		return browser.tabs.sendMessage(tabs[0].id, {action: "enableEventListener"})
+	})
+	.then(() => {
+		window.close()
 	})
 }
 
