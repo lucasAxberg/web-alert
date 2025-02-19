31967cb5e046cbe78a87a3c3b01157e0ab707048
Lucas Axberg <lucas.axberg@gmail.com>
2025-02-19 09:02:19 +0100

    feature: store the trackers on the server localy for later display on popup open

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,33 @@
+// Server settings
+let ip
+let port
+
+function data_exists(data) {
+    return data !== "" && data !== null
+}
+
+// Get data from server and save it in local storage as JSON string
+new Promise((resolve) => {
+	// Get ip and port
+	ip = localStorage.getItem("ip")
+	port = localStorage.getItem("port")
+	// Print if one of port or ip doesn't exist
+	if (data_exists(ip) === false && data_exists(port) === false) {
+		console.log("Must specify Port and IP")
+	}
+	resolve({ip, port})
+})
+.then(({ip, port}) => fetch("http://" + ip + ":" + port)) // Get data
+.then((response) => response.text()) // Get the text sent
+.then((data) => {
+	// Store data as JSON string
+	localStorage.setItem("trackers", data)	
+	console.log("Successfully stored data: " + data)
+})
+.catch((err) => {
+	console.log(err)
+})
+
 document.addEventListener("click", (e) => {
 	switch (e.target.id) {
 		case "add":
